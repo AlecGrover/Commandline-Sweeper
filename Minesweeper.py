57b363a469a1caf7ea9d44a07c495657c4ee7acc
AlecGrover <39810460+AlecGrover@users.noreply.github.com>
2020-03-26 17:04:19 -0700

    Uploaded importable Minesweeper class

---
+++ Minesweeper.py
@@ -0,0 +1,100 @@
+import random as r
+
+
+class Minesweeper:
+    def __init__(self, width=10, height=10, mines=10):
+        self._width = width
+        self._height = height
+        self._mines = mines
+        self._board = [[0 for y in range(height)] for x in range(width)]
+        self._turns = 0
+        self._max_turns = width * height - mines
+        self._display_board = [["?" for y in range(height)] for x in range(width)]
+
+    def generate_mines(self, x, y):
+        r.seed()
+        for i in range(self._mines):
+            rand_x = r.randrange(self._width)
+            rand_y = r.randrange(self._height)
+            while rand_x == x or rand_y == y or self._board[rand_x][rand_y] == -1:
+                rand_x = r.randrange(self._width)
+                rand_y = r.randrange(self._height)
+            self._board[rand_x][rand_y] = -1
+
+    def minesweep(self, x, y):
+        found_mines = 0
+
+        if x < self._width - 1:
+            if self._board[x + 1][y] == -1:
+                found_mines += 1
+            if y < self._height - 1 and self._board[x + 1][y + 1] == -1:
+                found_mines += 1
+            if y > 0 and self._board[x + 1][y - 1] == -1:
+                found_mines += 1
+        if x > 0:
+            if self._board[x - 1][y] == -1:
+                found_mines += 1
+            if y < self._height - 1 and self._board[x - 1][y + 1] == -1:
+                found_mines += 1
+            if y > 0 and self._board[x - 1][y - 1] == -1:
+                found_mines += 1
+        if y > 0 and self._board[x][y - 1] == -1:
+            found_mines += 1
+        if y < self._height - 1 and self._board[x][y + 1] == -1:
+            found_mines += 1
+
+        self._display_board[x][y] = found_mines
+        print("Found {0} mines adjacent to ({1}, {2})".format(found_mines, x, y))
+
+        if found_mines == 0:
+            if x < self._width - 1:
+                if self._display_board[x + 1][y] == "?":
+                    self.minesweep(x + 1, y)
+                if y > 0 and self._display_board[x + 1][y - 1] == "?":
+                    self.minesweep(x + 1, y - 1)
+                if y < self._height - 1 and self._display_board[x + 1][y + 1] == "?":
+                    self.minesweep(x + 1, y + 1)
+            if x > 0:
+                if self._display_board[x - 1][y] == "?":
+                    self.minesweep(x - 1, y)
+                if y > 0 and self._display_board[x - 1][y - 1] == "?":
+                    self.minesweep(x - 1, y - 1)
+                if y < self._height - 1 and self._display_board[x - 1][y + 1] == "?":
+                    self.minesweep(x - 1, y + 1)
+            if y > 0 and self._display_board[x][y - 1] == "?":
+                self.minesweep(x, y - 1)
+            if y < self._height - 1 and self._display_board[x][y + 1] == "?":
+                self.minesweep(x, y + 1)
+
+        return found_mines
+
+    def print_board(self):
+        for y in range(self._height - 1, -1, -1):
+            for x in range(self._width):
+                print(self._display_board[x][y], end="  ")
+            print()
+
+    def process_play(self, x, y):
+        if self._turns == 0:
+            self.generate_mines(x, y)
+            self._turns += 1
+            found = self.minesweep(x, y)
+            self.print_board()
+            return 0, self._turns
+        elif self._board[x][y] == -1:
+            print("Mine at ({0},{1})! Game Over!".format(x, y))
+            return 1, self._turns + 1
+        else:
+            found = self.minesweep(x, y)
+            self._turns += 1
+            self.print_board()
+            if self._turns == self._max_turns:
+                print("All safe spaces played, you win!")
+                return 2, self._turns
+            # print("Found {0} mines adjacent to ({1}, {2})".format(found, x, y))
+            return 0, self._turns
+
+
+# new_minesweeper = Minesweeper()
+# # new_minesweeper.print_board()
+# win, turns = new_minesweeper.process_play(0, 0)
