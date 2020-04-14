3e2a90c0b8037e840da830379bf0ba6582dc34e1
AlecGrover <39810460+AlecGrover@users.noreply.github.com>
2020-04-14 13:30:31 -0700

    Update to make generate.py more versatile
    Now contains optional boolean flags top, bottom, left, and right. When set to True generate will not output combinations featuring offsets that would break those boundaries.

--- generate.py
+++ generate.py
@@ -1,6 +1,32 @@
 import itertools
 
 
-def get_offsets(n):
-    offsets = [(1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1)]
-    return [offset_set for offset_set in itertools.combinations(offsets, n)]
+def get_offsets(n, top=False, bottom=False, left=False, right=False):
+    if not top and not bottom and not left and not right:
+        offsets = [(1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1)]
+        return [offset_set for offset_set in itertools.combinations(offsets, n)]
+    if top:
+        if right:
+            offsets = [(0, -1), (-1, -1), (-1, 0)]
+            return [offset_set for offset_set in itertools.combinations(offsets, n)]
+        if left:
+            offsets = [(1, 0), (1, -1), (0, -1)]
+            return [offset_set for offset_set in itertools.combinations(offsets, n)]
+        offsets = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)]
+        return [offset_set for offset_set in itertools.combinations(offsets, n)]
+    if bottom:
+        if right:
+            offsets = [(-1, 0), (-1, 1), (0, 1)]
+            return [offset_set for offset_set in itertools.combinations(offsets, n)]
+        if left:
+            offsets = [(1, 1), (1, 0), (0, 1)]
+            return [offset_set for offset_set in itertools.combinations(offsets, n)]
+        offsets = [(1, 1), (1, 0), (-1, 0), (-1, 1), (0, 1)]
+        return [offset_set for offset_set in itertools.combinations(offsets, n)]
+    if right:
+        offsets = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1)]
+        return [offset_set for offset_set in itertools.combinations(offsets, n)]
+    if left:
+        offsets = [(1, 1), (1, 0), (1, -1), (0, -1), (0, 1)]
+        return [offset_set for offset_set in itertools.combinations(offsets, n)]
+    
